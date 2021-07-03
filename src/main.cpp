51d43d29fb95fbf683a6bbc1f5898d99a66d7bed
giuliapezzutti <giulia.pezzutti@studenti.unipd.it>
2021-07-03 18:28:50 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+
+    String path_images("*.png");
+    String path = "data/"+path_images;
+
+    return 0;
+}
