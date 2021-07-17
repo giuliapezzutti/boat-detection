56459c4978c2f6ea5a99b2ea7d22af20cac6b874
Giulia Pezzutti <74766522+giuliapezzutti@users.noreply.github.com>
2021-07-17 16:35:14 +0200

    Update training.py

--- src/training.py
+++ src/training.py
@@ -38,7 +38,7 @@ def CNN(in_shape, out_shape):
     X = tf.keras.layers.Flatten()(X)
     X = tf.keras.layers.Dense(out_shape[0]*out_shape[1], activation='sigmoid', name='fc')(X)
     X = tf.keras.layers.Reshape(out_shape)(X)
-    x = tf.keras.layers.Lambda(activation)(X)
+    X = tf.keras.layers.Lambda(activation)(X)
 
     m = tf.keras.Model(inputs=X_input, outputs=X)
 
@@ -54,19 +54,36 @@ masks_path = glob(mask_folder + "*.png")
 dataset = []
 masks = []
 
-for label_path in images_path:
-    img = cv.imread(label_path, cv.CV_8UC4)
+folder = "data/training_dataset/processed_images/"
+mask_folder = "data/training_dataset/masks/"
+
+images_path = sorted(glob(folder + "*.png"))
+masks_path = sorted(glob(mask_folder + "*.png"))
+
+dataset = []
+masks = []
+
+tot = 3220
+for path in images_path:
+    img = cv.imread(path, cv.CV_8UC4)
     _, c2, c3, c4 = cv.split(img)
-    img = cv.merge((c2, c3, c4))
-    # img_resized = cv.resize(img, (128, 64))
-    dataset.append(img)
+    # c1, c2, c3, _ = cv.split(img)
+    img = cv.merge((c4, c2, c3))
+    # img = cv.cvtColor(img, cv.COLOR_HSV2BGR)
+    img = img / 255
+    img_resized = cv.resize(img, (224, 224))
+    dataset.append(img_resized)
+
 dataset = np.array(dataset)
 
-for label_path in masks_path:
-    mask = cv.imread(label_path, cv.CV_8UC1)
-    mask = mask/255
-    mask_resized = cv.resize(mask, (128, 256))
-    masks.append(mask)
+for path in masks_path:
+    mask = cv.imread(path, cv.CV_8UC1)
+    mask = mask / 255
+    # mask_resized = mask.reshape(0, 1)
+    mask_resized = cv.resize(mask, (224, 224))
+    np_mask = np.asarray(mask_resized).reshape(-1)
+    masks.append(np_mask)
+
 masks = np.array(masks)
 
 input_shape = dataset[0].shape
@@ -74,44 +91,62 @@ output_shape = masks[0].shape
 
 print('Input shape: ', input_shape)
 print('Output shape: ', output_shape)
+print('Number of images: ', len(dataset))
 
 train_dataset, val_dataset, train_labels, val_labels = train_test_split(dataset, masks, train_size=0.7, random_state=1)
 val_dataset, test_dataset, val_labels, test_labels = train_test_split(val_dataset, val_labels, train_size=0.7, random_state=1)
 
 early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 batch_size = 64
-num_epochs = 20
+num_epochs = 10
 
 # print("CNN ----------------")
 # model = CNN(input_shape, output_shape)
 
-# # print("VGG ----------------")
-# vgg = tf.keras.applications.VGG19(input_shape=input_shape, include_top=False, weights='imagenet')
+# print("VGG ----------------")
+# vgg = tf.keras.applications.VGG19(input_shape=(224, 224, 3), include_top=True, weights=None, classes=50176, classifier_activation='sigmoid')
 # x = Flatten()(vgg.output)
 # x = tf.keras.layers.Dense(output_shape[0]*output_shape[1], activation='sigmoid', name='fc')(x)
 # x = tf.keras.layers.Reshape(output_shape)(x)
 # x = tf.keras.layers.Lambda(activation)(x)
 # model = Model(vgg.input, x)
 
-print("Inception ----------------")
-vgg = tf.keras.applications.InceptionResNetV2(input_shape=input_shape, include_top=False)
-x = Flatten()(vgg.output)
-x = tf.keras.layers.Dense(output_shape[0]*output_shape[1], activation='sigmoid', name='fc')(x)
-x = tf.keras.layers.Reshape(output_shape)(x)
-x = tf.keras.layers.Lambda(activation)(x)
-model = Model(vgg.input, x)
+# print("Inception ----------------")
+# vgg = tf.keras.applications.InceptionResNetV2(input_shape=input_shape, include_top=False)
+# x = Flatten()(vgg.output)
+# x = tf.keras.layers.Dense(output_shape[0]*output_shape[1], activation='sigmoid', name='fc')(x)
+# x = tf.keras.layers.Reshape(output_shape)(x)
+# x = tf.keras.layers.Lambda(activation)(x)
+# model = Model(vgg.input, x)
 
-# model.compile(optimizer="adam", loss='binary_crossentropy', metrics=[tf.keras.metrics.MeanIoU(num_classes=2)])
-#
-# history = model.fit(x=train_dataset, y=train_labels, validation_data=(val_dataset, val_labels), epochs=num_epochs,
-#                     callbacks=[early_stop_callback])
+print("MobileNet ----------------")
+mobnet = tf.keras.applications.MobileNet(input_shape=input_shape, weights=None, include_top=True, classes=50176, classifier_activation='sigmoid')
+
+mobnet.compile(optimizer="adam", loss='binary_crossentropy', metrics=[tf.keras.metrics.MeanIoU(num_classes=2)])
+mobnet.summary()
+
+history = mobnet.fit(x=train_dataset, y=train_labels, validation_data=(val_dataset, val_labels), epochs=num_epochs,
+                    callbacks=[early_stop_callback])
 #
 # model.save('data/models/model.h5')
 
-pred = model.predict(dataset[9:10])
-plt.imshow(pred[0])
-plt.show()
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
-print(masks[9])
-plt.imshow(masks[9])
-plt.show()
+path = "model/"
+
+full_model = tf.function(lambda inputs: mobnet(inputs))
+full_model = full_model.get_concrete_function([tf.TensorSpec(model_input.shape, model_input.dtype) for model_input in mobnet.inputs])
+
+frozen_func = convert_variables_to_constants_v2(full_model)
+frozen_func.graph.as_graph_def()
+
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir=path+"frozen_models", name="boat.pb", as_text=False)
+# tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir=path+"frozen_models", name="boat.pbtxt", as_text=True)
+
+# pred = model.predict(dataset[9:10])
+# plt.imshow(pred[0])
+# plt.show()
+#
+# print(masks[9])
+# plt.imshow(masks[9])
+# plt.show()
