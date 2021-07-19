ba9c0bd8c63c4132c03047a78807a77bba96dd0d
giuliapezzutti <74766522+giuliapezzutti@users.noreply.github.com>
2021-07-19 17:31:00 +0200

    update training.py

--- src/training.py
+++ src/training.py
@@ -85,8 +85,6 @@ model.save('models/model.h5')
 
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
 
-path = "models/"
-
 full_model = tf.function(lambda inputs: model(inputs))
 full_model = full_model.get_concrete_function(
     [tf.TensorSpec(model_input.shape, model_input.dtype) for model_input in model.inputs])
@@ -94,5 +92,5 @@ full_model = full_model.get_concrete_function(
 frozen_func = convert_variables_to_constants_v2(full_model)
 frozen_func.graph.as_graph_def()
 
-tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir=path + "frozen_models", name="boat.pb", as_text=False)
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir="models/", name="model.pb", as_text=False)
 # tf.io.write_graph(graph_or_graph_def=frozen_func.graph, logdir=path+"frozen_models", name="boat.pbtxt", as_text=True)
